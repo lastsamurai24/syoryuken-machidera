4904d183191f0a020b2edf1a1f004cf6ce167348
samurai <lastsamurai24@icloud.com>
2023-08-29 10:50:08 +0900

    できました

--- syoryuken/ViewController.swift
+++ syoryuken/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var attackImage: UIImageView!
+    
+    var imageArrayAttack : Array<UIImage> = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        syoryu()
         // Do any additional setup after loading the view.
     }
 
-
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func attackButton(_ sender: Any) {
+        attackImage.animationImages = imageArrayAttack
+        attackImage.animationDuration = 2
+        attackImage.animationRepeatCount = 2
+        attackImage.startAnimating()
+    }
+    func syoryu(){
+        while let attackimage = UIImage(named: "attack\(imageArrayAttack.count + 1)"){
+            imageArrayAttack.append(attackimage)
+        }
+    }
 }
 
